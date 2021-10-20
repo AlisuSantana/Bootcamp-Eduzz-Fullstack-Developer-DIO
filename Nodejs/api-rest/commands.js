54129a19ad3a0eba6ce4bc86df565fe6adeaec52
Allyson Santana <santana.allyson886@gmail.com>
2021-10-20 10:42:12 -0300

    Modificando README.ms

--- Nodejs/api-rest/commands.js
+++ Nodejs/api-rest/commands.js
@@ -4,21 +4,18 @@
 ## Nesse projeto realizamos uma api REST (CRUD)
 
 
-/* Comandos & Dependencias instaladas durante o estudo *\
+`/* Comandos & Dependencias instaladas durante o estudo *\`
 
-/**
- * Nodejs
- * npm init
- * npm install -g typescript
- * npm install --save-dev typescript
- * tsc --init
- * npm install --save-dev @types/node * 
- * npm install --save-dev ts-node-dev * 
- */
+`Nodejs`
+`npm init`
+`npm install -g typescript`
+`npm install --save-dev typescript`
+`tsc --init`
+`npm install --save-dev @types/node *`
+`npm install --save-dev ts-node-dev *` 
 
-/**
- * OBS: scripts do package.json...
- * "start": "node ./",
- * "build": "tsc -p ."
- * "dev": "ts-node-dev --respawn --transpile-only --igonere-watch node_modules --no-notify src/index.ts"
- */
+
+`OBS: scripts do package.json...`
+`"start": "node ./",`
+`"build": "tsc -p ."`
+`"dev": "ts-node-dev --respawn --transpile-only --igonere-watch node_modules --no-notify src/index.ts"`
